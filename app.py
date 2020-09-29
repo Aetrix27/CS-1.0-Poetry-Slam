3e6a8a0783b1f81f5d1a52850776ead6b6384f6c
Aetrix27 <davedogg323@gmail.com>
2020-09-29 14:14:27 -0700

    Finished lines_printed_backwards
    Prints poem in reverse

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
 filename="poem.txt"
-
 infile = open(filename, "r")
 
 def get_file_lines(filename):
-
+    """This function gets a list of file lines from a text file"""
     for line in infile:
+        #split_line_list= line.rstrip().split("\n")
         return infile.readlines()
 
-print(get_file_lines(filename))
+#print(get_file_lines(filename))
+
+def lines_printed_backwards(lines_list):
+    counter=len(lines_list)
+    for line in reversed(lines_list):
+        counter-=1
+        print(f"{counter} {line}")
+
+    return
+    #return list(reversed(lines_list))
+    #return line.rstrip()
+
+#print(f.read)
+#.write()
+
+lines_printed_backwards(get_file_lines(filename))
+
+infile.close()
