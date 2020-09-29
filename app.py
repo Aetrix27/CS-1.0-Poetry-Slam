20846d37db59fb4d5937f13b42cb8ba87223a273
Aetrix27 <davedogg323@gmail.com>
2020-09-29 16:41:24 -0700

    Completed lines_printed_random
    Prints random lines in the poem

--- app.py
+++ app.py
@@ -1,27 +1,33 @@
+import random
+
 filename="poem.txt"
-infile = open(filename, "r")
+#infile = open(filename, "r")
 
 def get_file_lines(filename):
     """This function gets a list of file lines from a text file"""
-    for line in infile:
-        #split_line_list= line.rstrip().split("\n")
-        return infile.readlines()
-
-#print(get_file_lines(filename))
+    infile = open(filename, "r")
+    return infile.read().splitlines()
 
 def lines_printed_backwards(lines_list):
+    """This function prints a poem backwards, with a line counter in reverse"""
     counter=len(lines_list)
     for line in reversed(lines_list):
         counter-=1
         print(f"{counter} {line}")
 
     return
-    #return list(reversed(lines_list))
-    #return line.rstrip()
+
+
+def lines_printed_random(lines_list):
+    """This function prints lines in random order from the poem"""
+    for line in lines_list:
+        print_line=random.randint(0,len(lines_list)-1)
+        print(lines_list[print_line])
+
+    return
 
 #print(f.read)
 #.write()
 
 lines_printed_backwards(get_file_lines(filename))
-
-infile.close()
+lines_printed_random(get_file_lines(filename))
