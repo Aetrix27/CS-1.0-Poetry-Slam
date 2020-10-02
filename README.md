000a5f09e04774a379db4a3f83c0d811c4830a34
Aetrix27 <davedogg323@gmail.com>
2020-10-01 22:38:48 -0700

    Final Commit

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # CS-1.0-Poetry-Slam
  
 The poem is Yom Kippur by Adrienne Rich
+This program prints certain lines of a poem. The user can print backwards, random lines,
+and then every two lines.
--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import random
 
 filename="poem.txt"
-#infile = open(filename, "r")
 
 def get_file_lines(filename):
     """This function gets a list of file lines from a text file"""
@@ -10,36 +9,35 @@ def get_file_lines(filename):
 
 def lines_printed_backwards(lines_list):
     """This function prints a poem backwards, with a line counter in reverse"""
-    print("Running lines_printed_backwards")
+    print("Running lines_printed_backwards\n")
     counter=len(lines_list)
     for line in reversed(lines_list):
         counter-=1
         print(f"{counter} {line}")
+    print("\n")
 
     return
 
 def lines_printed_random(lines_list):
     """This function prints lines in random order from the poem"""
-    print("Running lines_printed_random")
+    print("Running lines_printed_random\n")
     for line in lines_list:
         print_line=random.randint(0,len(lines_list)-1)
         print(lines_list[print_line])
+    print("\n")
 
     return
 
 def lines_printed_custom(lines_list):
-    """This function print out every two lines"""
+    """This function prints out every two lines of the poem"""
     line_counter=0
-    print("Running lines_printed_custom")
+    print("Running lines_printed_custom\n")
     for line in lines_list:
         line_counter+=1
-        if (counter%2==0):
+        if (line_counter%2==0):
             print(line)
 
     return
-
-#print(f.read)
-#.write()
 
 lines_printed_backwards(get_file_lines(filename))
 lines_printed_random(get_file_lines(filename))
